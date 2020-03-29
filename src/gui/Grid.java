0c0783be74dcfdb66a684effd72f1ac3a96f5056
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-29 13:44:06 -0400

    Created class for the GUI

---
+++ src/gui/Grid.java
@@ -0,0 +1,18 @@
+package gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class Grid
+{
+	private JFrame gameFrame;
+	
+	public Grid()
+	{
+		gameFrame = new JFrame("Tic Tac Toe Frame");
+		gameFrame.setLayout(new BorderLayout());
+		
+		
+	}
+}

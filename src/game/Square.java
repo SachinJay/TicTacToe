7920ce940cac02a08a86b76d251deb7a0ba79e22
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-29 13:35:16 -0400

    Added comments

--- src/game/Square.java
+++ src/game/Square.java
@@ -1,5 +1,11 @@
 package game;
 
+/**
+ * Enum to represent the only three states a square on a tic tac toe board can be in 
+ * Either blank, x, or o
+ * @author Sachin Devasahayam
+ *
+ */
 public enum Square
 {
 	BLANK,

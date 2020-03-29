759466dd516d27e1573679389a558b5b5b128b53
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-29 13:08:46 -0400

    Implemented some more of Board class
    -Two main things
    --A convenience print method
    --A place method for placing a mark in a box

--- src/game/Board.java
+++ src/game/Board.java
@@ -4,6 +4,9 @@ public class Board
 {
 	private Square[][] board;
 	
+	/**
+	 * Constructor, creates a blank board
+	 */
 	public Board()
 	{
 		Square blank = Square.BLANK;
@@ -14,6 +17,38 @@ public class Board
 				sqr = blank;				
 			}
 		}
+	}
+	
+	/**
+	 * Prints current board
+	 */
+	public void print()
+	{
+		for(Square[] arr : board)
+		{
+			for(Square sqr : arr)
+			{
+				if(sqr.equals(Square.BLANK))
+				{
+					System.out.print("-"+" ");
+				}
+				{
+					System.out.print(sqr + " ");
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Board is indexed from top left down, so top row is row zero etc
+	 * E.x. place(Side.X,1,1) places an X in the center
+	 * @param square either an X or an O to be placed
+	 * @param r the row number 
+	 * @param c the column number
+	 */
+	public void place(Square square, int r, int c)
+	{
+		board[r][c] = square;
 	}
 
 }

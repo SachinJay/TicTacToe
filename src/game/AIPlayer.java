6be3714a8961c92150a6b3fec987cced5d0e86e0
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-30 17:22:36 -0400

    Commented minimax code for clarity and moved some things into constants

--- src/game/AIPlayer.java
+++ src/game/AIPlayer.java
@@ -15,15 +15,24 @@ public class AIPlayer extends Player
 	 * Performs minimax algo
 	 * @param depth depth of the search
 	 * @param curSide current side that is playing, either maximizer or minimizer
-	 * @param game
-	 * @return
+	 * @param game the current game
+	 * @return 3 element array of {score, row, col} where row and col are the row and column of the best move
 	 */
 	public int[] minimax(int depth, Square curSide, Game game)
 	{
+		//All possible next moves for curSide
 		List<int[]> moves = game.nextMoves(curSide);
+		//The opponent
 		Square opSide = curSide == Square.O ? Square.X : Square.O;
+		//Treat the invoking player as the maximizer
 		Square maximizer = this.getSide();
+		
+		//Initial value should either be as high or as low as possible, depending on whether 
+		//You are minimizing or maximizing. If you are maximizing, for example, the 
+		//init value should be as low as possible, that way the first evaluation is always bigger
+		//than it and thus is always initially picked
 		int maxMinEval = (curSide == maximizer) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+		
 		int eval;
 		int bestRow = -1;
 		int bestCol = -1;
@@ -31,24 +40,35 @@ public class AIPlayer extends Player
 		//If we reached end depth or the game is over
 		if(depth == 0 || game.isWon(curSide) || game.isWon(opSide))
 		{
+			//Just evaluate the current board configuration and the best move is whatever
+			//the best move is currently set to
 			int[] retArr = {game.getBoard().evaluate(maximizer), bestRow, bestCol};
 			return retArr;
 		}
+		
 		if(curSide == maximizer)
 		{
 			for(int[] move : moves)
 			{
+				//Perform the move
 				game.getBoard().place(curSide, move[0], move[1]);
+				
+				//perform minimax no this new configuration, record the score
 				eval = minimax(depth-1,opSide,game)[0];
+				
+				//if the score from the above minimax is better than your current score,
+				//then it is a better move and so we set the current score equal to it 
+				//and the current best move equal to it
 				if(eval > maxMinEval)
 				{
 					maxMinEval = eval;
 					bestRow = move[0];
 					bestCol = move[1];
 				}
-				//undo move
+				//undo move so we can look at more configurations
 				game.getBoard().place(Square.BLANK, move[0], move[1]);
 			}
+			
 			int retArr[] = {maxMinEval, bestRow, bestCol};
 			return retArr;
 		}
@@ -74,9 +94,17 @@ public class AIPlayer extends Player
 		
 	}
 
+	/**
+	 * Performs minimax at given depth and returns only the move from it
+	 * @param game the game that is being played
+	 * @return an int[] {row,col} where row,col are the coordinates of the best move
+	 */
 	public int[] move(Game game)
 	{
-		int[] miniMaxArr = this.minimax(2, this.getSide(), game);
+		//Perform minimax at given depth
+		int[] miniMaxArr = this.minimax(Constants.MINIMAX_DEPTH, this.getSide(), game);
+		
+		//Ignore the first element since that is the score
 		int[] retArr = {miniMaxArr[1], miniMaxArr[2]};
 		
 		return retArr;
--- src/game/Constants.java
+++ src/game/Constants.java
@@ -25,6 +25,7 @@ public class Constants
 	
 	//******Player******//
 	public static final String DEFAULT_PLAYER_NAME = "Sachin";
+	public static final int MINIMAX_DEPTH = 2;
 	
 	
 	//*****GUI**********/
@@ -37,14 +38,12 @@ public class Constants
 
 	//SquarePanel
 	public static final Dimension SQUARE_DIM = new Dimension(60,60);
-
 	public static final Dimension BOARD_DIM = new Dimension(450,450);
 
 	public static final String IMAGES_PATH ="src/images/";
 	public static final String IMAGE_SUFFIX = ".png";
 	
 	public static final Border BOARD_BORDER = new MatteBorder(5, 5, 5, 5, Color.BLACK);
-
 	public static final Border SQUARE00_BORDER = new MatteBorder(0, 0, 5, 5, Color.BLACK);
 	public static final Border SQUARE01_BORDER = new MatteBorder(0, 5, 5, 5, Color.BLACK);
 	public static final Border SQUARE02_BORDER = new MatteBorder(0, 5, 5, 0, Color.BLACK);
@@ -55,11 +54,8 @@ public class Constants
 	public static final Border SQUARE21_BORDER = new MatteBorder(5, 5, 0, 5, Color.BLACK);
 	public static final Border SQUARE22_BORDER = new MatteBorder(5, 5, 0, 0, Color.BLACK);
 
-	public static final Color STATUS_BACKGROUND = Color.white;
-			//Color.decode("0x89CFF0");
-
+	public static final Color STATUS_BACKGROUND = Color.decode("0x89CFF0"); //Baby blue
 	public static final Font STATUS_FONT = new Font("Times New Roman", 1, 20);
-
 	public static final Dimension STATUS_DIM = new Dimension(450,30);
 	
 }
